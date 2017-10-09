ac1fdb124bc93afe80e2e0f8adeac34f8890a4e1
solobat <solopea@gmail.com>
2017-10-09 22:25:08 +0800

    v2.0.2

--- extension/js/info/changelog.js
+++ extension/js/info/changelog.js
@@ -1,5 +1,9 @@
 let langZh = [
     {
+        version: 'v2.0.2',
+        detail: "设置页新增单词编辑功能； <br /> 一些UI样式优化。"
+    },
+    {
         version: 'v2.0.1',
         detail: "标签添加<em>自动提示</em>功能; 修正level筛选逻辑; <br /> 一些UI样式优化。"
     },
