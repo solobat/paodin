99d1bb2c0a350ac4213a89bed94a6ac762c0d312
solobat <solopea@gmail.com>
2017-10-11 01:36:15 +0800

    version 2.0.3

--- extension/js/info/changelog.js
+++ extension/js/info/changelog.js
@@ -1,7 +1,7 @@
 let langZh = [
     {
         version: 'v2.0.3',
-        detail: "查词时带出已有单词的tags以及释义; <br />一些UI样式优化。"
+        detail: "查词时带出已有单词的tags以及释义; <br />一些交互及UI样式优化。"
     },
     {
         version: 'v2.0.2',
